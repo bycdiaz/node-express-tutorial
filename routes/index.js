fde543a0776abf894d7d18759b7ee5ffae1b4273
Carlos Diaz <cdiaz86@gmail.com>
2020-12-12 16:40:07 -0800

    Adds params and response practice.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const carlos = {
+    name: "Carlos",
+    age: 100,
+    cool: true
+  }
+  res.json(carlos);
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reversedName = [...req.params.name].reverse('').join('');
+  res.send(reversedName);
+})
 
 module.exports = router;
