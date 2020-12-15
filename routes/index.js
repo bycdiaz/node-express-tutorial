939f4b57125152eb98e1ca5b360b56f609bd7194
Carlos Diaz <cdiaz86@gmail.com>
2020-12-14 20:07:55 -0800

    Passed / request to controller.

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const helpers = require('../helpers');
+const storeController = require('../controllers/storeController');
 
-// Do work here
-router.get('/', (req, res) => {
-  // const carlos = {
-  //   name: "Carlos",
-  //   age: 100,
-  //   cool: true
-  // }
-  // res.json(carlos);
-
-  res.render('hello', {
-    title: 'I Love Food',
-    saleEnd: helpers.moment().endOf('day').fromNow()
-  });
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reversedName = [...req.params.name].reverse('').join('');
-  res.send(reversedName);
-})
+router.get('/', storeController.homePage);
 
 module.exports = router;
