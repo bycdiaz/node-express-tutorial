6a6b2bd84eaa6da607e6b9983725202e41e084a4
Carlos Diaz <cdiaz86@gmail.com>
2020-12-15 21:04:12 -0800

    Removes unused code.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,10 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Carlos';
-  res.cookie('name', 'Carlos', { maxAge: 900000000 })
-  next();
-}
-
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index', { title: 'I Love Food'});
 }
