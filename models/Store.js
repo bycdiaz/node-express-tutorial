807c7f0ceb8811f4e905957352e74ae95a79bab2
Carlos Diaz <cdiaz86@gmail.com>
2020-12-15 21:04:29 -0800

    Adds store schema.

---
+++ models/Store.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+// Tells mongoose we're using built-in ES6 promise
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name!'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
+})
+
+module.exports = mongoose.model('Store', storeSchema);
