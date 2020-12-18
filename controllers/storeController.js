377a81e16631a2594d15819302be89ebae365424
Carlos Diaz <cdiaz86@gmail.com>
2020-12-17 23:28:59 -0800

    Adds flash message on successful store creation.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
+  req.flash('error', 'Something Happened');
+  req.flash('info', 'Something Happened');
+  req.flash('warning', 'Something Happened');
+  req.flash('success', 'Something Happened');
+
   res.render('index', { title: 'I Love Food'});
 }
 
@@ -12,5 +17,7 @@ exports.addStore = (req, res) => {
 exports.createStore = async (req, res) => {
   const store = new Store(req.body);
   await store.save();
+  req.flash('success', `Successfully Created ${store.name}.
+  Care to leave a review.`);
   res.redirect('/');
 }
